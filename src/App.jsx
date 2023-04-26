f01d0e5392c68c7c60c98a3e2a54be7b6b74f3b3
Sermadurai1997 <124354534+Sermadurai1997@users.noreply.github.com>
2023-04-26 14:45:10 +0530

    committed

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Topbar from "./components/topbar/Topbar" ;
+import Intro from "./components/intro/Intro" ;
+import Portfolio from "./components/portfolio/Portfolio" ;
+import Testimonial from "./components/testimonial/Testimonial" ;
+import Works from "./components/works/Works" ;
+import Contact from "./components/contact/Contact" ;
+
+
+function App() {
+  return (
+    <div className="app"> {/*whole container*/}
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Testimonial/>
+        <Contact/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
